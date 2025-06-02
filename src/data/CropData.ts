a92283e921a0d4568cfc3fec946b2a933dd5b852
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-02 14:29:24 +0100

    Data files for animals and crops

---
+++ src/data/CropData.ts
@@ -0,0 +1,50 @@
+export const cropData = [{
+    name: "Wheat",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/wheat.svg",
+    alt: "A graphic of wheat."
+},
+{
+    name: "Sweetcorn",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/sweetcorn.svg",
+    alt: "A graphic of a sweetcorn."
+},
+{
+    name: "Strawberry",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/strawberry.svg",
+    alt: "A graphic of a strawberry."
+},
+{
+    name: "Potato",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/potato.svg",
+    alt: "A graphic of a potato."
+},
+{
+    name: "Tomato",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/tomato.svg",
+    alt: "A graphic of a red tomato."
+},
+{
+    name: "Apple",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/apple.svg",
+    alt: "A graphic of a red apple."
+},
+{
+    name: "Carrot",
+    buyPrice: 25,
+    sellPrice: 75,
+    img: "src/assets/Crops/carrot.svg",
+    alt: "A graphic of a carrot."
+}
+]
