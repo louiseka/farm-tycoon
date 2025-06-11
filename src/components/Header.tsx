87c56a3eafa9e8f033094facdcbee3b19a6deb36
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-11 15:55:08 +0100

    Updating img paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ export default function Header() {
         <header>
             <h1> Farm Tycoon</h1>
             <nav>
-                <NavLink to="/"><span><img className="nav-icon" src="public/assets/farmer-icon.svg" alt="Graphic of a old man farmer."></img></span>Dashboard</NavLink>
-                <NavLink to="/shop"><span><img className="nav-icon" src="public/assets/shop-icon.svg" alt="Graphic of a shopping trolley."></img></span>Shop</NavLink>
-                <NavLink to="/farm"><span><img className="nav-icon" src="./assets/farm-icon.svg" alt="A graphic of a farm barn."></img></span>Farm</NavLink>
-                <NavLink to="/stats"><span><img className="nav-icon" src="./assets/lightbulb-icon.svg" alt="A graphic of a switched on light bulb."></img></span>Stats</NavLink>
+                <NavLink to="/"><span><img className="nav-icon" src="../assets/Icons/farmer-icon.svg" alt="Graphic of a old man farmer."></img></span>Dashboard</NavLink>
+                <NavLink to="/shop"><span><img className="nav-icon" src="../assets/Icons/shop-icon.svg" alt="Graphic of a shopping trolley."></img></span>Shop</NavLink>
+                <NavLink to="/farm"><span><img className="nav-icon" src="../assets/Icons/farm-icon.svg" alt="A graphic of a farm barn."></img></span>Farm</NavLink>
+                <NavLink to="/stats"><span><img className="nav-icon" src="../assets/Icons/lightbulb-icon.svg" alt="A graphic of a switched on light bulb."></img></span>Stats</NavLink>
             </nav>
         </header>
     )
