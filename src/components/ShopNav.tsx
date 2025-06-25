473416a406127dc414c53afbd97553a14640d986
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-25 16:03:57 +0100

    Adding logic for active links

--- src/components/ShopNav.tsx
+++ src/components/ShopNav.tsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router"
 export default function ShopNav() {
     return (
         <nav className="nested-nav" aria-label="Shop navigation">
-            <NavLink to="shop/crops"><img className="nav-icon" src="../assets/Icons/crops.svg" alt="Graphic of a vegetable patch." aria-hidden="true"></img>Crops</NavLink>
-            <NavLink to="shop/animals"><img className="nav-icon" src="../assets/Icons/animals.svg" alt="Graphic of a farmer pulling a trailer of farm animals with his tractor." aria-hidden="true"></img>Animals</NavLink>
+            <NavLink to="shop/crops" className={({ isActive }) => (isActive ? 'nav-active' : 'nav-unactive')}><img className="nav-icon" src="../assets/Icons/crops.svg" alt="Graphic of a vegetable patch." aria-hidden="true"></img>Crops</NavLink>
+            <NavLink to="shop/animals" className={({ isActive }) => (isActive ? 'nav-active' : 'nav-unactive')}><img className="nav-icon" src="../assets/Icons/animals.svg" alt="Graphic of a farmer pulling a trailer of farm animals with his tractor." aria-hidden="true"></img>Animals</NavLink>
         </nav>
     )
 }
